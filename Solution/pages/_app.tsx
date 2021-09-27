39112097d211dabc7c7415652409c35c7a5b6bb2
ArkuniGIT <arkuniweb@gmail.com>
2021-09-27 16:16:50 +0200

    Added Roboto font

--- Solution/pages/_app.tsx
+++ Solution/pages/_app.tsx
@@ -1,13 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Layout from '../components/layout/layout';
 
 const App = ({ Component, pageProps }: AppProps) =>
 {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
 
     );
 }
