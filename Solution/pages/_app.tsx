9d74973fdf02e26baa46f18d890900606e76ef9a
ArkuniGIT <arkuniweb@gmail.com>
2021-09-28 09:00:55 +0200

    Added Progress Bar when loading

--- Solution/pages/_app.tsx
+++ Solution/pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Router from 'next/router';
 import Head from 'next/head'
 import Layout from '../components/layout/Layout';
 import React from 'react';
 import { CssBaseline } from '@mui/material';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());  
 
 const App = ({ Component, pageProps }: AppProps) =>
 {
