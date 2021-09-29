b3491770b3eef69bb6f1f94d283a6906f4d39f65
ArkuniGIT <arkuniweb@gmail.com>
2021-09-29 15:23:43 +0200

    Used next.js image

--- Solution/pages/index.tsx
+++ Solution/pages/index.tsx
@@ -3,12 +3,19 @@ import { Box } from '@mui/system'
 import type { NextPage } from 'next'
 import Link from "next/link";
 import React from 'react'
+import Image from 'next/image'
 
 const HomePage: NextPage = () =>
 {
     return (
         <Box textAlign="center">
-            <img className={"rounded"} src="https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_1050.jpg" />
+            <Image 
+                width={500}
+                height={375}
+                className={"rounded"} 
+                src="https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_1050.jpg" 
+                alt=""
+            />
             <br />
             <br />
             <Typography variant="h3" gutterBottom>
